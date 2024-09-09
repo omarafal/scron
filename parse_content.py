ac243038c361274b261fd1c17900f584e6ca05e5
Omar Ashraf <114801560+omarafal@users.noreply.github.com>
2024-09-09 23:25:08 +0300

    add more options

--- parse_content.py
+++ parse_content.py
@@ -4,9 +4,13 @@ content = sys.stdin.read()
 
 lines = content.split("\n")
 
-with open("cmds.tmp", "w") as f:
+with open("cmds.tmp", "w") as f1, open("current_cmds.scron", "a") as f2:
     for line in lines:
         # ignore empty lines:
         if line.strip() != "":
             #print(f"{line} >> ./logs/scron_commands.log\n")
-            f.write(f"{line} >> ./logs/scron_output.log 2>> ./logs/scron_error.log\n")
+            f1.write(f"{line} >> ./logs/scron_output.log 2>> ./logs/scron_error.log\n")
+
+            # add commands to list of commands disregarding the logging part
+            # used for -l option
+            f2.write(f"{line}\n")
