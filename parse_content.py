1ddc27bba39d2691e28d2ae3d275274f44de90e2
Omar Ashraf <114801560+omarafal@users.noreply.github.com>
2024-09-10 22:57:15 +0300

    add initial time parsing

--- parse_content.py
+++ parse_content.py
@@ -9,8 +9,36 @@ time = sys.argv[2]
 with open(file, "r") as read_file:
     lines = read_file.read().split("\n")
 
+# PARSE TIME
+split_time = time.split("@")
+
+if len(split_time) != 2:
+    print("[ERROR] Wrong timing format.\nExample usage: scron -t \"23 Jan @ 8:30 AM\" -f file.txt")
+    exit()
+
+f_half = split_time[0]
+s_half = split_time[1]
+
+# parse first half
+# split first half
+splf_half = f_half.split(" ")
+
+if len(splf_half) == 1:
+    # either a number (1-31) or day (Sun, Mon, Tue, Wed, Thu, Fri, Sat)
+   if splf_half[0] in range(1, 32):
+       pass
+   elif splf_half[0] in ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]:
+       pass
+   else:
+       print("[ERROR] Wrong timing format.\nExample usage: scron -t \"23 Jan @ 8:30 AM\" -f file.txt")
+       exit()
+
+
+"""
 # prepare commands and write them to files
 with open("./c/cmds.tmp", "w") as f1, open("./c/current_cmds.scron", "a") as f2:
+    # log dates
+    f1.write("date >> ./logs/scron_output.log && date >> ./logs/scron_error.log\n")
     for line in lines:
         # ignore empty lines:
         if line.strip() != "":
@@ -20,4 +48,4 @@ with open("./c/cmds.tmp", "w") as f1, open("./c/current_cmds.scron", "a") as f2:
             # add commands to list of commands disregarding the logging part
             # used for -l option
             f2.write(f"{line}\n")
-            
+   """         
