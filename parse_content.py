6a8e125a0cbeb89b2178dc25733d93efc4954c99
Omar Ashraf <114801560+omarafal@users.noreply.github.com>
2024-09-11 16:08:41 +0300

    handle parsing a singular time for the half before @

--- parse_content.py
+++ parse_content.py
@@ -5,6 +5,10 @@ import sys
 file = sys.argv[1]
 time = sys.argv[2]
 
+time_list =  ["*", "*", "*", "*", "*"]
+
+days_list =  ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
 # read lines from file
 with open(file, "r") as read_file:
     lines = read_file.read().split("\n")
@@ -21,17 +25,23 @@ s_half = split_time[1]
 
 # parse first half
 # split first half
-splf_half = f_half.split(" ")
+splf_half = f_half.split()
 
 if len(splf_half) == 1:
     # either a number (1-31) or day (Sun, Mon, Tue, Wed, Thu, Fri, Sat)
-   if splf_half[0] in range(1, 32):
-       pass
-   elif splf_half[0] in ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]:
-       pass
-   else:
-       print("[ERROR] Wrong timing format.\nExample usage: scron -t \"23 Jan @ 8:30 AM\" -f file.txt")
-       exit()
+    if splf_half[0] in [str(x) for x in range(1, 32)]:
+        #print("This is a number")
+        time_list[2] = splf_half[0]
+        #print(f"{" ".join(time_list)}")
+        #pass
+    elif splf_half[0] in days_list:
+        #print("This is a day of the week")
+        time_list[4] = str(days_list.index(splf_half[0]))
+        print(f"{" ".join(time_list)}")
+        pass
+    else:
+        print("[ERROR] Wrong timing format.\nExample usage: scron -t \"23 Jan @ 8:30 AM\" -f file.txt")
+        exit()
 
 
 """
